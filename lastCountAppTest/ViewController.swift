4b5aece6ac0e9144c645f294bef032af8a6d7997
nttr <nttr@BI-23559.local>
2017-08-02 14:27:17 +0900

    add count func

--- lastCountAppTest/ViewController.swift
+++ lastCountAppTest/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    var num: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        num += 1
+        label.text = String(num)
+    }
+
+    @IBAction func minus(){
+        num -= 1
+        label.text = String(num)
+    }
+    @IBAction func reset(){
+        num = 0
+        label.text = String(num)
+    }
 
 }
 
